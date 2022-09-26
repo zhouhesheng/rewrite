3c8b5a3b869ba485fc98a2e9e5cd72dea22b2158
Zhou He Sheng <hszhou@ilegendsoft.com>
2022-09-26 18:34:58 +0800

    update

--- rewrite.go
+++ rewrite.go
@@ -1,11 +1,10 @@
 package rewrite
 
-import "net/http"
-import "net/url"
-import "strings"
-import "regexp"
-import "path"
-import "fmt"
+import (
+	"net/http"
+	"net/url"
+	"regexp"
+)
 
 const headerField = "X-Rewrite-Original-URI"
 
@@ -15,13 +14,7 @@ type Rule struct {
 	*regexp.Regexp
 }
 
-var regfmt = regexp.MustCompile(`:[^/#?()\.\\]+`)
-
 func NewRule(pattern, to string) (*Rule, error) {
-	pattern = regfmt.ReplaceAllStringFunc(pattern, func(m string) string {
-		return fmt.Sprintf(`(?P<%s>[^/#?]+)`, m[1:])
-	})
-
 	reg, err := regexp.Compile(pattern)
 	if err != nil {
 		return nil, err
@@ -35,65 +28,20 @@ func NewRule(pattern, to string) (*Rule, error) {
 }
 
 func (r *Rule) Rewrite(req *http.Request) bool {
-	oriPath := req.URL.Path
-
+	oriPath := req.URL.String()
 	if !r.MatchString(oriPath) {
 		return false
 	}
 
-	to := path.Clean(r.Replace(req.URL))
-
-	u, e := url.Parse(to)
+	to := r.ReplaceAllString(oriPath, r.To)
+	uo, e := url.Parse(to)
 	if e != nil {
 		return false
 	}
 
-	req.Header.Set(headerField, req.URL.RequestURI())
-
-	req.URL.Path = u.Path
-	req.URL.RawPath = u.RawPath
-	if u.RawQuery != "" {
-		req.URL.RawQuery = u.RawQuery
-	}
-
+	req.URL.Path = uo.Path
+	req.URL.RawPath = uo.RawPath
 	return true
-}
-
-func (r *Rule) Replace(u *url.URL) string {
-	if !hit("\\$|\\:", r.To) {
-		return r.To
-	}
-
-	uri := u.RequestURI()
-
-	regFrom := regexp.MustCompile(r.Pattern)
-	match := regFrom.FindStringSubmatchIndex(uri)
-
-	result := regFrom.ExpandString([]byte(""), r.To, uri, match)
-
-	str := string(result[:])
-
-	if hit("\\:", str) {
-		return r.replaceNamedParams(uri, str)
-	}
-
-	return str
-}
-
-var urlreg = regexp.MustCompile(`:[^/#?()\.\\]+|\(\?P<[a-zA-Z0-9]+>.*\)`)
-
-func (r *Rule) replaceNamedParams(from, to string) string {
-	fromMatches := r.FindStringSubmatch(from)
-
-	if len(fromMatches) > 0 {
-		for i, name := range r.SubexpNames() {
-			if len(name) > 0 {
-				to = strings.Replace(to, ":"+name, fromMatches[i], -1)
-			}
-		}
-	}
-
-	return to
 }
 
 func NewHandler(rules map[string]string) RewriteHandler {
